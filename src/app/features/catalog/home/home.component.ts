80ee177b7279a0b875bdf922385e6ca1c4c8d930
yellendhar08 <yellendharlodi225@gmail.com>
2026-05-01 11:47:30 +0530

    Added banner images in home

--- src/app/features/catalog/home/home.component.ts
+++ src/app/features/catalog/home/home.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, OnDestroy, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 import { CatalogService } from '../../../core/services/catalog.service';
 import { ProductCardComponent } from '../product-card/product-card.component';
 import { Product } from '../../../core/models/product.model';
@@ -13,13 +13,33 @@ import { Category } from '../../../core/models/category.model';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   private catalogService = inject(CatalogService);
+  private router = inject(Router);
 
   featuredProducts: Product[] = [];
   categories: Category[] = [];
 
+  // Carousel Logic
+  slides = [
+    { image: 'assets/images/slide1.png', categoryId: 2, label: 'Mobiles' },
+    { image: 'assets/images/slide2.png', categoryId: 3, label: 'Clothing' },
+    { image: 'assets/images/slide3.png', categoryId: 4, label: 'Furniture' }
+  ];
+  currentSlide = 0;
+  slideInterval: any;
+
   ngOnInit() {
+    this.fetchFeaturedProducts();
+    this.fetchCategories();
+    this.startAutoSlide();
+  }
+
+  ngOnDestroy() {
+    this.stopAutoSlide();
+  }
+
+  fetchFeaturedProducts() {
     this.catalogService.getFeaturedProducts().subscribe({
       next: (res: any) => {
         if (res.success && res.data) {
@@ -27,13 +47,51 @@ export class HomeComponent implements OnInit {
         }
       }
     });
+  }
 
+  fetchCategories() {
     this.catalogService.getCategories().subscribe({
       next: (res: any) => {
         if (res.success && res.data) {
           this.categories = res.data;
         }
       }
+    });
+  }
+
+  startAutoSlide() {
+    this.slideInterval = setInterval(() => {
+      this.nextSlide();
+    }, 4000);
+  }
+
+  stopAutoSlide() {
+    if (this.slideInterval) {
+      clearInterval(this.slideInterval);
+    }
+  }
+
+  nextSlide() {
+    this.currentSlide = (this.currentSlide + 1) % this.slides.length;
+  }
+
+  prevSlide() {
+    this.currentSlide = (this.currentSlide - 1 + this.slides.length) % this.slides.length;
+  }
+
+  goToSlide(index: number) {
+    this.currentSlide = index;
+    this.resetAutoSlide();
+  }
+
+  resetAutoSlide() {
+    this.stopAutoSlide();
+    this.startAutoSlide();
+  }
+
+  onSlideClick(categoryId: number) {
+    this.router.navigate(['/customer/products'], { 
+      queryParams: { categoryId, sortBy: 'newest' } 
     });
   }
 
@@ -46,7 +104,8 @@ export class HomeComponent implements OnInit {
       groceries: '🛒',
       footwear: '👟',
       sports: '⚽',
-      books: '📚'
+      books: '📚',
+      'skin care': '🧴'
     };
 
     return icons[name.toLowerCase()] || '🏷️';
